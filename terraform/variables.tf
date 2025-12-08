c032796172636db880375d164d4bf5ea60a8b3f6
Daniel <danielbossi1981@gmail.com>
2025-12-08 21:25:37 +0000

    cambiar imagen linux

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,5 +19,5 @@ variable "instance_type" {
 variable "ami_id" {
 	description = "ID de la imagen Ubuntu 22.04"
 	type = string
-	default = "ami-0c398cb65a93047f2"
+	default = "ami-01872345de812817d"
 }
